6b96b7ed27ca26e463f5aa6ba1cf08c71643ecce
maxexcloo <max@excloo.com>
2011-12-03 16:17:03 +1100

    Cleaned Up

--- rename.sh
+++ rename.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# A simple script that goes through a list of torrents and renames them to the names listed in their metadata.
-
 cd $(dirname $0)
-FILES=Music/*
 
-for f in $FILES
+# Loop Through Files
+for file in torrents/*.torrent
 do
-	python2 torrentinfo.py "$f" >> list.txt
-	echo "$f"
+	# Print Torrent Metadata Name To File
+	python2 torrentinfo.py "$file" >> list.txt
+	# Echo File Name
+	echo "$file"
 done
