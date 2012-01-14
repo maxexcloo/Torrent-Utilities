9581916267c64dea627428611a9ecb7811d6ce01
maxexcloo <max@excloo.com>
2012-01-14 15:46:52 +1100

    Renamed Lists

--- unique.sh
+++ unique.sh
@@ -4,19 +4,19 @@ cd $(dirname $0)
 # Loop Through Files
 for file in *.torrent; do
 	# Add Current File To List
-	echo "$file" | sed 's/ (.*).torrent/.torrent/g' >> album.list
+	echo "$file" | sed 's/ (.*).torrent/.torrent/g' >> list.album
 done
 
 # Loop Through Files (Year)
 for file in *.torrent; do
 	# Add Current File To List
-	echo "$file" | sed 's/ - [0-9][0-9][0-9][0-9] (.*).torrent/.torrent/g' >> year.list
+	echo "$file" | sed 's/ - [0-9][0-9][0-9][0-9] (.*).torrent/.torrent/g' >> list.year
 done
 
 # Sort Lists
-sort -o album.list album.list
-sort -o year.list year.list
+sort -o list.album list.album
+sort -o list.year list.year
 
 # Print Unique List Values
-uniq -u album.list > album.list
-uniq -u year.list > year.list
+uniq -u list.album > list.album
+uniq -u list.year > list.year
