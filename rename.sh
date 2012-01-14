2af53b159b9cbea6522001d4db90b203c5b3c4e0
maxexcloo <max@excloo.com>
2012-01-14 15:43:29 +1100

    Added Uniqueness Checker

--- rename.sh
+++ rename.sh
@@ -2,8 +2,7 @@
 cd $(dirname $0)
 
 # Loop Through Files
-for file in torrents/*.torrent
-do
+for file in torrents/*.torrent; do
 	# Print Torrent Metadata Name To File
 	python2 torrentinfo.py "$file" >> list.txt
 	# Echo File Name
