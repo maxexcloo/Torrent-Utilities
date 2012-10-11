3d9fee14ce1c1b60257858d986b1a24263c3cfd1
Max Schaefer <max@excloo.com>
2012-10-11 21:05:26 +1100

    Cleaned Up

--- rtorrent-checker/xmlrpc2scgi.py
+++ rtorrent-checker/xmlrpc2scgi.py
@@ -6,12 +6,12 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -50,7 +50,7 @@ def do_scgi_xmlrpc_request(host, methodname, params=()):
     xmlreq = xmlrpclib.dumps(params, methodname)
     xmlresp = SCGIRequest(host).send(xmlreq)
     #~ print xmlresp
-    
+
     return xmlresp
 
 def do_scgi_xmlrpc_request_py(host, methodname, params=()):
@@ -67,36 +67,36 @@ def do_scgi_xmlrpc_request_py(host, methodname, params=()):
 class SCGIRequest(object):
     """ See spec at: http://python.ca/scgi/protocol.txt
         Send an SCGI request.
-        
+
         Use tcp socket
         SCGIRequest('scgi://host:port').send(data)
-        
+
         Or use the named unix domain socket
         SCGIRequest('scgi:///tmp/rtorrent.sock').send(data)
     """
-    
+
     def __init__(self, url):
         self.url=url
         self.resp_headers=[]
-    
+
     def __send(self, scgireq):
         scheme, netloc, path, query, frag = urlparse.urlsplit(self.url)
         host, port = urllib.splitport(netloc)
         #~ print '>>>', (netloc, host, port)
-        
+
         if netloc:
             addrinfo = socket.getaddrinfo(host, port, socket.AF_INET, socket.SOCK_STREAM)
-            
+
             assert len(addrinfo) == 1, "There's more than one? %r"%addrinfo
             #~ print addrinfo
-            
+
             sock = socket.socket(*addrinfo[0][:3])
             sock.connect(addrinfo[0][4])
         else:
             # if no host then assume unix domain socket
             sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
             sock.connect(path)
-        
+
         sock.send(scgireq)
         recvdata = resp = sock.recv(1024)
         while recvdata != '':
@@ -105,24 +105,24 @@ class SCGIRequest(object):
             resp += recvdata
         sock.close()
         return resp
-    
+
     def send(self, data):
         "Send data over scgi to url and get response"
         scgiresp = self.__send(self.add_required_scgi_headers(data))
         resp, self.resp_headers = self.get_scgi_resp(scgiresp)
         return resp
-    
+
     @staticmethod
     def encode_netstring(string):
         "Encode string as netstring"
         return '%d:%s,'%(len(string), string)
-    
+
     @staticmethod
     def make_headers(headers):
         "Make scgi header list"
         #~ return '\x00'.join(headers)+'\x00'
         return '\x00'.join(['%s\x00%s'%t for t in headers])+'\x00'
-    
+
     @staticmethod
     def add_required_scgi_headers(data, headers=[]):
         "Wrap data in an scgi request,\nsee spec at: http://python.ca/scgi/protocol.txt"
@@ -131,11 +131,11 @@ class SCGIRequest(object):
             ('CONTENT_LENGTH', str(len(data))),
             ('SCGI', '1'),
         ] + headers)
-        
+
         enc_headers = SCGIRequest.encode_netstring(headers)
-        
+
         return enc_headers+data
-    
+
     @staticmethod
     def gen_headers(file):
         "Get header lines from scgi response"
@@ -143,7 +143,7 @@ class SCGIRequest(object):
         while line.strip():
             yield line
             line = file.readline().rstrip()
-    
+
     @staticmethod
     def get_scgi_resp(resp):
         "Get xmlrpc response from scgi response"
@@ -152,7 +152,7 @@ class SCGIRequest(object):
         for line in SCGIRequest.gen_headers(fresp):
             #~ print "Header: %r"%line
             headers.append(line.split(': ', 1))
-        
+
         xmlresp = fresp.read()
         return (xmlresp, headers)
 
@@ -165,11 +165,11 @@ class RTorrentXMLRPCClient(object):
         if rtc.d.get_ratio(infohash) > 500:
             print "%s has a ratio of over 0.5"%(rtc.d.get_name(infohash))
     """
-    
+
     def __init__(self, url, methodname=''):
         self.url = url
         self.methodname = methodname
-    
+
     def __call__(self, *args):
         #~ print "%s%r"%(self.methodname, args)
         scheme, netloc, path, query, frag = urlparse.urlsplit(self.url)
@@ -184,7 +184,7 @@ class RTorrentXMLRPCClient(object):
             raise Exception('Unsupported protocol')
         else:
             raise Exception('Unsupported protocol')
-    
+
     def __getattr__(self, attr):
         methodname = self.methodname and '.'.join([self.methodname,attr]) or attr
         return RTorrentXMLRPCClient(self.url, methodname)
@@ -198,7 +198,7 @@ def convert_params_to_native(params):
         if len(param) < 2 or param[1] != '/':
             cparams.append(param)
             continue
-        
+
         if param[0] == 'i':
             ptype = int
         elif param[0] == 'b':
@@ -208,24 +208,24 @@ def convert_params_to_native(params):
         else:
             cparams.append(param)
             continue
-        
+
         cparams.append(ptype(param[2:]))
-    
+
     return tuple(cparams)
 
 def main(argv):
     output_python=False
-    
+
     if argv[0] == '-p':
         output_python=True
         argv.pop(0)
-    
+
     host, methodname = argv[:2]
-    
+
     respxml = do_scgi_xmlrpc_request(host, methodname,
                                     convert_params_to_native(argv[2:]))
     #~ respxml = RTorrentXMLRPCClient(host, methodname)(convert_params_to_native(argv[2:]))
-    
+
     if not output_python:
         print respxml
     else:
